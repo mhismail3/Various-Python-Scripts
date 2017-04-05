4cf5616bf1679737cd1c9d0c501fbc618db42e13
mhismail3 <mhismail3@gmail.com>
2017-04-05 14:42:47 -0500

    Rename

--- get-file-info.py
+++ get-file-info.py
@@ -1,32 +1,29 @@
-import os
-import sys
-import stat
-import time
+import os, sys, stat, time
 
-file_name = input("> ")
+fileName = input("> ")
 try:
-    file_stats = os.stat(file_name)
+    fileStats = os.stat(fileName)
 except OSError:
     print ("\nNameError : No such file or directory\n")
 
 
-file_info = {
-    'fname': file_name,
-    'fsize': file_stats.st_size,
-    'f_lm' : time.strftime("%d/%m/%Y %I:%M:%S %p", time.localtime(file_stats.st_mtime)),
-    'f_la' : time.strftime("%d/%m/%Y %I:%M:%S %p", time.localtime(file_stats.st_atime)),
-    'f_ct' : time.strftime("%d/%m/%Y %I:%M:%S %p", time.localtime(file_stats.st_ctime))
+fileInfo = {
+    'fname': fileName,
+    'fsize': fileStats.st_size,
+    'f_lm' : time.strftime("%d/%m/%Y %I:%M:%S %p", time.localtime(fileStats.st_mtime)),
+    'f_la' : time.strftime("%d/%m/%Y %I:%M:%S %p", time.localtime(fileStats.st_atime)),
+    'f_ct' : time.strftime("%d/%m/%Y %I:%M:%S %p", time.localtime(fileStats.st_ctime))
 }
 
 print (("\nfile name = {fname}, file size = {fsize} bytes, last modified = {f_lm}, " +
-       "last accessed = {f_la}, creation time = {f_ct}\n").format(**file_info))
+       "last accessed = {f_la}, creation time = {f_ct}\n").format(**fileInfo))
 
-if stat.S_ISDIR(file_stats.st_mode):
+if stat.S_ISDIR(fileStats.st_mode):
     print ("This a directory")
 else:
     print ("This is not a directory\n")
-    print ("A closer look at the os.stat({}) tuple:".format(file_name))
-    print (file_stats)
+    print ("A closer look at the os.stat({}) tuple:".format(fileName))
+    print (fileStats)
     print ("\nThe above tuple has the following sequence:")
     print ("""st_mode (protection bits), st_ino (inode number),
     st_dev (device), st_nlink (number of hard links),
